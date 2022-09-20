b8f6f8dfbbba8340918451e90f4383a03a05db49
Anandhan <anandhangithub@gmail.com>
2022-09-20 12:36:24 +0530

    add import export

--- student_scores/accounts/admin.py
+++ student_scores/accounts/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from accounts.models import students_data
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+
+
 # Register your models here.
+class StudentResource(resources.ModelResource):
+
+    class Meta:
+        model = students_data
+        import_id_fields = ('roll_no',)
 
 
-class StudentsDataAdmin(students_data):
-    list_filter = ('roll_no', 'team_no', 'score')
-    list_display = ('name', 'roll_no', 'score', 'team_no')
+class StudentsDataAdmin(ImportExportModelAdmin,students_data):
+    list_filter = ('roll_no', 'team_no', 'score',)
+    list_display = ('name', 'roll_no', 'score', 'team_no',)
 
 
 # admin.site.register(students_data)
---
+++ student_scores/accounts/urls.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.urls import path
+
+urls_pattern = [ 
+    path('1/', admin.site.urls)
+    ]
